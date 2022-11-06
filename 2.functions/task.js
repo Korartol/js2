7bef4be315601cdc4eaf33875e472c9571f8b800
Артем <artem@MacBook-Pro-Artem.local>
2022-11-06 18:04:26 +0300

    ДЗ по функциям

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,48 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity; 
+  let sum = 0;
+  let avg = 0;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    sum += arr[i];
+  };
+
+  avg = +(sum / arr.length).toFixed(2);  // округлям до двух знаков после запятной и преобразуем к number (+)
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  };
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  // Ваш код
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum1 = func(arrOfArr[i]);
+    if (sum1 > max) {
+      max = sum1;
+    }
+  }
   
   return max;
 }
@@ -28,4 +50,20 @@ function makeWork(arrOfArr, func) {
 // Задание 3
 function worker2(arr) {
   // Ваш код
+    let max = -Infinity;
+    let min = Infinity;
+    let different;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      };
+      if (arr[i] < min) {
+        min = arr[i];
+      };
+    };
+    different = Math.abs(max - min);
+
+    return different;
+
 }
