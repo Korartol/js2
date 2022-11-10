6a92442747da1248b6a5010e024321de79be3c5c
Артем <artem@MacBook-Pro-Artem.local>
2022-11-10 15:34:51 +0300

    DZ arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(arr0 => arr0 > 0).filter(arr0 => arr0 % 3 === 0).map(arr0 => arr0 * 10);
 
   return resultArr; // array
 }
