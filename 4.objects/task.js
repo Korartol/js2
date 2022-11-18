2d5b1e6ea4dbf7142fab94b9f866870f325652ff
Артем <artem@MacBook-Pro-Artem.local>
2022-11-18 09:08:05 +0300

    ДЗ объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined){
+    this.marks = [mark];
+    
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined){
+    this.marks = [...mark];
+    
+  } else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+
+  let sum = this.marks.reduce((acc, mark) => acc + mark);
+
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject
+  this.excluded = reason;
+}
+
 
 // ваш код для остальных методов
